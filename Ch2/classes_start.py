15a12bd491bdce9f7a07a7ef7e9b36c7a9054141
Manish <mp121995@gmail.com>
2020-02-22 13:48:47 -0800

    Classes

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -6,10 +6,27 @@ class myClass():
   def method1(self):
     print("myClass method1")
 
-  def method2
+  def method2(self, someString):
+    print("myClass method2 "+ someString)
+
+
+class anotherClass():
+  def method1(self):
+    myClass.method1(self)
+    print("anotherClass method1")
+
+  def method2(self, someString):
+    print("anotherClass method2 "+ someString)
 
 
 def main():
+  # c1 = myClass()
+  # c1.method1()
+  # c1.method2("This is a String")
+
+  c2 = anotherClass()
+  c2.method1()
+  c2.method2("This is a String")
 
   
 if __name__ == "__main__":
