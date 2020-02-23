9a0e6f886a27e82437d6e1e7bb9d3d85d6c9bc99
Manish <mp121995@gmail.com>
2020-02-23 14:40:42 -0800

    Fetching internet data

--- Ch4/ospathutils_start.py
+++ Ch4/ospathutils_start.py
@@ -10,12 +10,18 @@ import time
 
 def main():
   # Print the name of the OS
+  # print(os.name)
 
 
   # Check for item existence and type
+  # print("Item exists : " + str(path.exists("textfile.txt")))
+  # print("Item is a file : " + str(path.isfile("textfile.txt")))
+  # print("Item is a directory : " + str(path.isdir("textfile.txt")))
 
   
   # Work with file paths
+  print("Item's path : " + str(path.realpath("textfile.txt")))
+  print("Item's path and name : " + str(path.split(path.realpath("textfile.txt"))))
 
   
   # Get the modification time
